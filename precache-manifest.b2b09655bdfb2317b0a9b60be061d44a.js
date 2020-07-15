c774d2ab1769c7fb98388308a2ca0b7326aec804
Derrick Abankwa <dkabankwa@yahoo.com>
2020-07-14 23:45:00 -0700

    Updates

---
+++ precache-manifest.b2b09655bdfb2317b0a9b60be061d44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b3512e26f5df97adabd2a27866c0ba",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "5ee3818c6ab537b7a81a",
+    "url": "/react-todo/static/js/2.aa2a7966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.aa2a7966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae3c4f6b9f8f3bfe3ec",
+    "url": "/react-todo/static/js/main.d44e3e2b.chunk.js"
+  },
+  {
+    "revision": "7d29887965cf3bc26674",
+    "url": "/react-todo/static/js/runtime-main.c0759490.js"
+  }
+]);
